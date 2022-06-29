43f5c3745ea881dfc8e6878639d151f0fe604cbd
santakratos <santakratos10@gmail.com>
2022-06-30 00:00:48 +0300

    v1.0

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-export const hotel = new Object();
+export const hotel = {
+  quantityOfPlaces: 30,
+  priceByPlace: 20,
+  bankAccount: 0,
+  guests: {},
+  getLength: function () {
+    return Object.keys(this.guests).length;
+  },
+  getActualFreePlace: function () {
+    return (this.quantityOfPlaces -= this.getLength());
+  },
+  paidPerPlace: function () {
+    return (this.bankAccount += this.priceByPlace);
+  },
+  checkInGuest: function (firstName, lastName, money) {
+    if (this.quantityOfPlaces < 1) {
+      return 'Sorry, we have not free spaces';
+    } else if (money < this.priceByPlace) {
+      return 'Sorry, you have not enough money';
+    } else {
+      const newQuest = (this.guests[this.getLength()] = {
+        firstName: firstName,
+        lastName: lastName,
+        money: money - this.priceByPlace,
+      });
+      this.quantityOfPlaces -= 1;
+      this.paidPerPlace();
+      return newQuest;
+    }
+  },
+};
